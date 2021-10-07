024e516c57a4dd002f596373f4698b3cfe291c3a
Abdullah Al Adwani <abdullahmmaladwani@gmail.com>
2021-10-07 17:59:58 +0300

    Finshed the Game

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,22 +3,27 @@ import WinnerNumber from "./WinnerNumber";
 
 function Home() {
   const randomNumber = Math.floor(Math.random() * 100 + 1);
-  
+
   return (
-    
     <div className="home">
-      
       <h1>Deliver the Mail package</h1>
-      
+
       <WinnerNumber randomNumber={randomNumber} />
       <h4>
-        You have been assigned to deliver a package with a beeping sound,<h5>To one of my closest <strong>"friends"</strong> who lives in the tallest
-        skyscraper in the World and works for our top competetors, but I don't know which floor.</h5>
+        You have been assigned to deliver a package with a beeping sound,
+        <h5>
+          To one of my closest <strong>"friends"</strong> he lives in the
+          tallest skyscraper in the City and he works for our top competetors,
+          but I don't know which floor his apartment is.
+        </h5>
       </h4>
-      <p>One of the feature of being a great Mailman is to be super lucky</p>
+      <p>One of the features of being a great Mailman is to be super lucky</p>
       <style>
-@import url('https://fonts.googleapis.com/css2?family=Akronim&family=Birthstone&family=Staatliches&display=swap');
-</style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Akronim&family=Birthstone&family=Staatliches&display=swap');
+        @import
+        url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+      </style>
     </div>
   );
 }
--- src/Components/WinnerNumber.js
+++ src/Components/WinnerNumber.js
@@ -4,13 +4,34 @@ import { useState } from "react";
 /* let attempts = 5; */
 
 function WinnerNumber({ randomNumber }) {
-
   const [winnerNumber, setWinnerNumber] = useState(0);
   const [attempts, setAttempts] = useState(5);
   const [status, setStatus] = useState("");
-  const [result, setResult] = useState("")
-  const [hint, setHint] = useState("")
-  
+  const [result, setResult] = useState("");
+  const [hint, setHint] = useState("");
+
+  const weekUser = () => {
+    if (17 >= +randomNumber && +randomNumber >= 1) {
+      setHint(
+        <h2>
+          You need help what are you a <em>"child"</em>
+        </h2>
+      );
+    } else if (60 >= +randomNumber && +randomNumber >= 18) {
+      setHint(
+        <h2>
+          I didn't expect an <em>"adult"</em> would press the Hint button
+        </h2>
+      );
+    } else if (100 >= +randomNumber && +randomNumber >= 61) {
+      setHint(
+        <h2>
+          even though I respect the <em>"Eldery"</em> I won't give you a Hint
+        </h2>
+      );
+    }
+  };
+
   const compare = () => {
     setAttempts(attempts - 1);
     console.log(winnerNumber, randomNumber, attempts);
@@ -21,19 +42,22 @@ function WinnerNumber({ randomNumber }) {
       setStatus("Higher");
     }
     if (+winnerNumber === +randomNumber && attempts >= 0) {
-          if (attempts >= 3) {
-            setResult(<h1> You survived with no injuries </h1>)
-            setStatus("")
-            
-            
-
-          } else if (attempts < 3) {
-           setResult(<h1> You survived with minimum injuries </h1>)
-           setStatus("")
-          }
+      if (attempts >= 3) {
+        setResult(<h1> You survived with no injuries </h1>);
+        setStatus("");
+        setAttempts("");
+        setHint("");
+      } else if (attempts < 3) {
+        setResult(<h1> You survived with minimum injuries </h1>);
+        setStatus("");
+        setAttempts("");
+        setHint("");
+      }
     } else if (attempts <= 0) {
       setResult(<h1>RIP</h1>);
-      setStatus("")
+      setStatus("");
+      setAttempts("");
+      setHint("");
     }
   };
 
@@ -41,14 +65,14 @@ function WinnerNumber({ randomNumber }) {
     window.location.reload(false);
   };
   return (
-    
-    <div className = "playerInput">
+    <div className="playerInput">
       {attempts > 0 ? <p>You have {attempts} Attempts</p> : ""}
 
       <p>{status}</p>
       <p>{result}</p>
-      
-      <input className = "userInput"
+      <p>{hint}</p>
+      <input
+        className="userInput"
         onChange={(event) => setWinnerNumber(event.target.value)}
         id="randomInput"
         type="Number"
@@ -59,9 +83,8 @@ function WinnerNumber({ randomNumber }) {
       <br />
       <button onClick={compare}>Submit</button>
       <button onClick={refreshPage}>Reset</button>
-      
+      <button onClick={weekUser}>Hint</button>
     </div>
-   
   );
 }
 
