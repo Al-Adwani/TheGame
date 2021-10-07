cbf5e25ee16981e756d1a874b4b4dfe40a230a29
Abdullah Al Adwani <abdullahmmaladwani@gmail.com>
2021-10-07 16:10:36 +0300

    Hint Needed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import WinnerNumber from "./WinnerNumber";
 
 function Home() {
-  const randomNumber = Math.floor(Math.random() * 20 + 1);
+  const randomNumber = Math.floor(Math.random() * 100 + 1);
   
   return (
     
@@ -12,8 +12,8 @@ function Home() {
       
       <WinnerNumber randomNumber={randomNumber} />
       <h4>
-        You are a Mailman who has to deliver package with a beeping sound,<h5>To one of my closest <strong>"friends"</strong> who lives in the tallest
-        skyscraper in the World and works for our top competetors, but you don't know which floor</h5>
+        You have been assigned to deliver a package with a beeping sound,<h5>To one of my closest <strong>"friends"</strong> who lives in the tallest
+        skyscraper in the World and works for our top competetors, but I don't know which floor.</h5>
       </h4>
       <p>One of the feature of being a great Mailman is to be super lucky</p>
       <style>
--- src/Components/WinnerNumber.js
+++ src/Components/WinnerNumber.js
@@ -1,30 +1,39 @@
 import React from "react";
 import { useState } from "react";
+
 /* let attempts = 5; */
 
 function WinnerNumber({ randomNumber }) {
+
   const [winnerNumber, setWinnerNumber] = useState(0);
   const [attempts, setAttempts] = useState(5);
   const [status, setStatus] = useState("");
-  const [hint, setHint] = useState("");
-
+  const [result, setResult] = useState("")
+  const [hint, setHint] = useState("")
+  
   const compare = () => {
     setAttempts(attempts - 1);
     console.log(winnerNumber, randomNumber, attempts);
     if (+winnerNumber - +randomNumber > 0) {
-      setStatus("you are high");
+      setStatus("Lower");
     }
     if (+randomNumber - +winnerNumber > 0) {
-      setStatus("you are lower");
+      setStatus("Higher");
     }
     if (+winnerNumber === +randomNumber && attempts >= 0) {
-          if (attempts > 3) {
-            alert("you won with high score")
+          if (attempts >= 3) {
+            setResult(<h1> You survived with no injuries </h1>)
+            setStatus("")
+            
+            
+
           } else if (attempts < 3) {
-            alert("you won with low score")
+           setResult(<h1> You survived with minimum injuries </h1>)
+           setStatus("")
           }
     } else if (attempts <= 0) {
-      alert("You lost");
+      setResult(<h1>RIP</h1>);
+      setStatus("")
     }
   };
 
@@ -32,24 +41,27 @@ function WinnerNumber({ randomNumber }) {
     window.location.reload(false);
   };
   return (
+    
     <div className = "playerInput">
-      {attempts > 0 ? <p>{attempts} Attempts Remaining</p> : "You Lost"}
+      {attempts > 0 ? <p>You have {attempts} Attempts</p> : ""}
 
       <p>{status}</p>
-
-      <input
+      <p>{result}</p>
+      
+      <input className = "userInput"
         onChange={(event) => setWinnerNumber(event.target.value)}
         id="randomInput"
         type="Number"
         min="1"
         max="20"
-        placeholder="1 to 20"
+        placeholder="1 to 100"
       />
       <br />
       <button onClick={compare}>Submit</button>
       <button onClick={refreshPage}>Reset</button>
       
     </div>
+   
   );
 }
 
